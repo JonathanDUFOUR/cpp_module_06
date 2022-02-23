a245a2c8fc78e56178500c0397699fb920aba790
jodufour <jodufour@student.42.fr>
2022-02-23 20:49:25 +0000

    Fix unused dynamic cast result [OK]

--- ex02/srcs/identify.cpp
+++ ex02/srcs/identify.cpp
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 05:32:47 by jodufour          #+#    #+#             */
-/*   Updated: 2022/02/23 20:38:19 by jodufour         ###   ########.fr       */
+/*   Updated: 2022/02/23 20:48:40 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,19 @@ void	identify(Base *p)
 	std::cout << "Address identification:" << std::endl;
 	try
 	{
-		dynamic_cast<A &>(*p);
+		(void)dynamic_cast<A &>(*p);
 		std::cout << "\t" "identified: A" << std::endl;
 	}
 	catch (std::exception const &e) {}
 	try
 	{
-		dynamic_cast<B &>(*p);
+		(void)dynamic_cast<B &>(*p);
 		std::cout << "\t" "identified: B" << std::endl;
 	}
 	catch (std::exception const &e) {}
 	try
 	{
-		dynamic_cast<C &>(*p);
+		(void)dynamic_cast<C &>(*p);
 		std::cout << "\t" "identified: C" << std::endl;
 	}
 	catch (std::exception const &e) {}
@@ -44,19 +44,19 @@ void	identify(Base &p)
 	std::cout << "Reference identification:" << std::endl;
 	try
 	{
-		dynamic_cast<A &>(p);
+		(void)dynamic_cast<A &>(p);
 		std::cout << "\t" "identified: A" << std::endl;
 	}
 	catch (std::exception const &e) {}
 	try
 	{
-		dynamic_cast<B &>(p);
+		(void)dynamic_cast<B &>(p);
 		std::cout << "\t" "identified: B" << std::endl;
 	}
 	catch (std::exception const &e) {}
 	try
 	{
-		dynamic_cast<C &>(p);
+		(void)dynamic_cast<C &>(p);
 		std::cout << "\t" "identified: C" << std::endl;
 	}
 	catch (std::exception const &e) {}
